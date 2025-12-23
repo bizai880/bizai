dbfa888a0f19b898268be4e1b5ba4da42d2044cd
bizai880 <anemia-fretted-jab@duck.com>
2025-12-23 15:57:01 +0400

    Add files via upload

---
+++ apps/web/lib/database/index.ts
@@ -0,0 +1,24 @@
+export class Database {
+  private static instance: Database;
+
+  private constructor() {}
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  async query(sql: string, params: any[] = []) {
+    console.log('Database query:', sql, params);
+    return { rows: [], rowCount: 0 };
+  }
+
+  async connect() {
+    console.log('Database connected');
+    return this;
+  }
+}
+
+export const db = Database.getInstance();
