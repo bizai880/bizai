ce087752c63dc6e4d8f28a7f4461f89b7028b3cc
bizai880 <anemia-fretted-jab@duck.com>
2025-12-24 04:00:39 +0000

    fix: update packages and configs

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,14 +1,14 @@
 // Main exports for @bizai/shared
 
 // Types
-export * from './types/common';
+export * from "./types/common";
 
 // Utilities
-export * from './utils/response';
-export * from './utils/validation';
+export * from "./utils/response";
+export * from "./utils/validation";
 
 // Services
-export * from './lib/cache';
-export * from './lib/supabase/server';
-export * from './lib/excel/generator';
-export * from './lib/storage/uploader';
+export * from "./lib/cache";
+export * from "./lib/supabase/server";
+export * from "./lib/excel/generator";
+export * from "../../../apps/web/lib/storage/uploader";
