ccb16a9bb3f3c9f539e3a9fa75f64f0ae895ffe0
bizai880 <anemia-fretted-jab@duck.com>
2025-12-30 23:37:08 +0400

    Create index.ts

---
+++ apps/modelhub/src/cubes/integration/SalesOutlookAutomation/index.ts
@@ -0,0 +1,34 @@
+import { SalesOutlookAutomationCube } from './processor';
+import { BaseCube } from '../../../core/BaseCube';
+
+// تصدير المكعب كافتراضي
+export default SalesOutlookAutomationCube;
+
+// تصدير الدوال المساعدة
+export { 
+  parseExcelFile, 
+  createSampleExcelFile 
+} from './excel-parser';
+
+export { 
+  generateFollowUpEmail,
+  generateDeliveryAlertEmail,
+  generatePriorityAlertEmail,
+  generateEscalationEmail
+} from './email-templates';
+
+export {
+  scheduleAutomation,
+  calculateNextRun,
+  stopScheduler
+} from './scheduler';
+
+export type {
+  SalesRecord,
+  EmailRecipient,
+  AlertRule,
+  EmailConfig,
+  AutomationConfig,
+  AlertResult,
+  CubeExecutionResult
+} from './types';
