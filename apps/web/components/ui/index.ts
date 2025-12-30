384dd6dcc9564ff12e11556b99cafec029f03ed0
bizai880 <anemia-fretted-jab@duck.com>
2025-12-30 12:05:43 +0400

    Create index.ts

---
+++ apps/web/components/ui/index.ts
@@ -0,0 +1,37 @@
+// تصدير جميع مكونات واجهة المستخدم
+export { Input } from "./input";
+export { Textarea } from "./textarea";
+export { Label } from "./label";
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+} from "./select";
+export { Switch } from "./switch";
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "./tabs";
+export { Badge } from "./badge";
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from "./dropdown-menu";
+export { Toaster } from "./sonner";
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card";
+export { Button, buttonVariants } from "./button";
